9524ab2c2fdf0f25b06fb97d1337cc973d3599ab
bartlomiej.kachnic@student.uj.edu.pl <you@example.com>
2023-05-06 09:32:49 +0200

    Added initTriangle func

--- webgl/scripts/app.js
+++ webgl/scripts/app.js
@@ -1,15 +1,98 @@
-const initTriangle = function() {
-    let canvas = document.getElementById('canvas_triangle');
-    let gl = canvas.getContext('webgl');
+const initTriangle = function () {
+  let canvas = document.getElementById("canvas_triangle");
+  let gl = canvas.getContext("webgl");
 
-    if (!gl)
-        alert("Webgl not supported!");
+  if (!gl) alert("Webgl not supported!");
 
-    gl.clearColor(0.8, 0.6, 0.2, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clearColor(0.8, 0.6, 0.2, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertexShader, vertexShaderTxt);
+  gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
+
+  let program = gl.createProgram();
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  gl.detachShader(program, vertexShader);
+  gl.detachShader(program, fragmentShader);
+
+  gl.validateProgram(program);
+
+  let triangleVert = [
+    0.0, 0.5, 1.0, 0.0, 0.0, -0.5, -0.5, 0.0, 1.0, 0.0, 0.5, -0.5, 0.0, 0.0,
+    1.0,
+  ];
+
+  const triangleVertexBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(triangleVert),
+    gl.STATIC_DRAW
+  );
+
+  const posAttrLocation = gl.getAttribLocation(program, "vertPosition");
+  const colorAttrLocation = gl.getAttribLocation(program, "vertColor");
+
+  gl.vertexAttribPointer(
+    posAttrLocation,
+    2,
+    gl.FLOAT,
+    gl.FALSE,
+    5 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.vertexAttribPointer(
+    colorAttrLocation,
+    3,
+    gl.FLOAT,
+    gl.FALSE,
+    5 * Float32Array.BYTES_PER_ELEMENT,
+    2 * Float32Array.BYTES_PER_ELEMENT
+  );
+
+  gl.enableVertexAttribArray(posAttrLocation);
+  gl.enableVertexAttribArray(colorAttrLocation);
+
+  gl.useProgram(program);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 };
 
+const initCanvas = function () {
+  initTriangle();
+};
 
-const initCanvas = function() {
-    initTriangle();
-}
+const vertexShaderTxt = `
+    precision mediump float;
+
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+    
+    varying vec3 fragColor;
+
+    void main()
+    {
+        fragColor = vertColor;
+        gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }
+`;
+
+const fragmentShaderTxt = `
+    precision mediump float;
+    varying vec3 fragColor;
+
+    void main()
+    {
+        gl_FragColor  = vec4(fragColor,1.0);
+    }
+`;
