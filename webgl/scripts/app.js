8b1c558846d4a62c443e9b7524551e9d2bdfd1a6
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-05-06 10:12:40 +0200

    Added initSquare func

--- webgl/scripts/app.js
+++ webgl/scripts/app.js
@@ -95,7 +95,84 @@ const initTriangle = function () {
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 };
 
+
+const initSquare = function () {
+  let canvas = document.getElementById("canvas_square");
+  let gl = canvas.getContext("webgl");
+
+  if (!gl) alert("Webgl not supported!");
+
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertexShader, vertexShaderTxt);
+  gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
+
+  let program = gl.createProgram();
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  gl.detachShader(program, vertexShader);
+  gl.detachShader(program, fragmentShader);
+
+  gl.validateProgram(program);
+
+  let triangleVert = [
+     // Triangle 1
+    -0.5, -0.5, 1.0, 0.0, 0.0, // X Y R G B
+    -0.5, 0.5, 0.0, 1.0, 0.0, 
+    0.5, -0.5, 0.0, 0.0, 1.0,
+     // Triangle 2
+    -0.5, 0.5, 0.0, 1.0, 0.0, 
+    0.5, -0.5, 0.0, 0.0, 1.0,
+    0.5, 0.5, 1.0, 0.0, 0.0,
+  ];
+
+  let triangleVertexBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(triangleVert),
+    gl.STATIC_DRAW
+  );
+
+  const posAttrLocation = gl.getAttribLocation(program, "vertPosition");
+  const colorAttrLocation = gl.getAttribLocation(program, "vertColor");
+
+  gl.vertexAttribPointer(
+    posAttrLocation,
+    2,
+    gl.FLOAT,
+    gl.FALSE,
+    5 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.vertexAttribPointer(
+    colorAttrLocation,
+    3,
+    gl.FLOAT,
+    gl.FALSE,
+    5 * Float32Array.BYTES_PER_ELEMENT,
+    2 * Float32Array.BYTES_PER_ELEMENT
+  );
+
+  gl.enableVertexAttribArray(posAttrLocation);
+  gl.enableVertexAttribArray(colorAttrLocation);
+
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+};
+
 const initCanvas = function () {
   initTriangle();
+  initSquare();
 };
 
